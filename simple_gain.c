432bdae8728a6dd73daf883110364a4868386e92
Eric Fontaine <eric@ericfontainejazz.com>
2020-11-29 02:39:39 -0500

    input amplitude display bar

--- simple_gain.c
+++ simple_gain.c
@@ -17,16 +17,22 @@ jack_port_t *input_port;
 jack_port_t *output_port;
 jack_client_t *client;
 
-float dBgain = 0.0f;
-float lineargain = 1.0f;
+float dBmakeupgain = 0.0f; // user inputs gain in dB
+float linearmakeupgain = 1.0f; // automatically calculated
+
+float dBcompressorthreshold = -20.0f; // user input in dB
+float linearcompressorthreshold = 0.1f; // automatically calculated
+
+float compressorratio = 1.0f;
+
+float maxamplitude = 0.0f;
+
+int maxrows, maxcols;
 
 /**
  * The process callback for this JACK application is called in a
  * special realtime thread once for each audio cycle.
  *
- * This client does nothing more than copy data from its input
- * port to its output port. It will exit when stopped by 
- * the user (e.g. using Ctrl-C on a unix-ish operating system)
  */
 int
 process (jack_nframes_t nframes, void *arg)
@@ -38,8 +44,15 @@ process (jack_nframes_t nframes, void *arg)
 
 	int i;
 
-	for (i = 0; i < nframes; i++)
-		out[i] = in[i] * lineargain;
+	for (i = 0; i < nframes; i++) {
+		if (in[i] < linearcompressorthreshold)
+			out[i] = in[i] * linearmakeupgain;
+		else
+			out[i] = (in[i] + (in[i] - linearcompressorthreshold) / compressorratio) * linearmakeupgain; // apply gain
+
+		if (fabs(in[i]) > maxamplitude)
+			maxamplitude = fabs(in[i]);
+	}
 
 	return 0;      
 }
@@ -62,7 +75,15 @@ main (int argc, char *argv[])
 	const char *server_name = NULL;
 	jack_options_t options = JackNullOption;
 	jack_status_t status;
-	
+
+	// ncurses setup
+	initscr(); // ncurses init terminal
+	getmaxyx(stdscr, maxrows, maxcols);
+	cbreak; // only input one character at a time
+	noecho(); // disable echoing of typed keyboard input
+	keypad(stdscr, TRUE); // allow capture of special keystrokes, like arrow keys
+	nodelay(stdscr, true); // make getch() non-blocking (to allow realtime output)
+
 	/* open a client connection to the JACK server */
 
 	client = jack_client_open (client_name, options, &status, server_name);
@@ -158,37 +179,87 @@ main (int argc, char *argv[])
 	free (ports);
 
 
-	// ncurses setup
-	initscr(); // ncurses init terminal
-	cbreak; // only input one character at a time
-	noecho(); // disable echoing of typed keyboard input
-	keypad(stdscr, TRUE); // allow capture of special keystrokes, like arrow keys
-
-	mvprintw( 0, 0, "Keyboard Usage: UP/DOWN arrows to increase/decrease gain.");
-
 	/* keep running until stopped by the user */
 	while (true) {
-		mvprintw( 1, 0, "gain: %+1.2f dB", dBgain);
+		erase(); // clear screen
+
+		mvprintw( 0, 0, "%1.4f ", maxamplitude);
+		int nfullchars = maxamplitude  * (float) (maxcols - 6);
+
+		int i;
+		for ( i=0; i < nfullchars; i++)
+			addch(ACS_CKBOARD);
+
+		maxamplitude = 0.0f;
+
+		mvprintw( 2, 0, "%+1.2f dB makeup gain (adjust with UP/DOWN)", dBmakeupgain);
+		mvprintw( 3, 0, "%+1.2f dB compressor threshold (adjust with t/g)", dBcompressorthreshold);
+		mvprintw( 4, 0, "%+1.2f compressor ratio (adjust with r/f)", compressorratio);
+
 		int keystroke = getch();
-		switch (keystroke) {
+		if (keystroke != ERR) {
+		  switch (keystroke) {
 
 			case KEY_UP:
-			dBgain += 1.0f;
+			dBmakeupgain += 1.0f;
+			break;
+
+			case KEY_RIGHT:
+			dBmakeupgain += 0.1f;
 			break;
 
 			case KEY_DOWN:
-			dBgain -= 1.0f;
+			dBmakeupgain -= 1.0f;
 			break;
+
+			case KEY_LEFT:
+			dBmakeupgain -= 0.1f;
+			break;
+
+			case 't':
+			dBcompressorthreshold += 1.0f;
+			break;
+
+			case 'T':
+			dBcompressorthreshold += 0.1f;
+			break;
+
+			case 'g':
+			dBcompressorthreshold -= 1.0f;
+			break;
+
+			case 'G':
+			dBcompressorthreshold -= 0.1f;
+			break;
+
+			case 'r':
+			compressorratio += 1.0f;
+			break;
+
+			case 'R':
+			compressorratio += 0.1f;
+			break;
+
+			case 'f':
+			compressorratio -= 1.0f;
+			break;
+
+			case 'F':
+			compressorratio -= 0.1f;
+			break;
+		  }
 		}
 			
-		if (dBgain > 0.0f)
-			mvprintw( 2, 0, " warning: gain exceeds 0 dB...be careful of clipping!\n");
+		if (dBmakeupgain > 0.0f)
+			mvprintw( 5, 0, " warning: makeup gain exceeds 0 dB...be careful of clipping!\n");
 
-		lineargain = pow(10, dBgain/20.0f); // linear gain = 10 ^ (dB/20)
+		// calculate linear from 10 ^ (dB/20)
+		linearmakeupgain = pow(10, dBmakeupgain/20.0f);
+		linearcompressorthreshold = pow(10, dBcompressorthreshold/20.0f);
 
 		clrtobot(); // clear rest of screen
 
-//		usleep (100000);
+		usleep (10000); // sleep in microseconds
 	}
 
 	/* this is never reached but if the program
